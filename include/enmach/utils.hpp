66421b9c8182c5f2f8b34ba6c1d613892bbc6dbc
marcope-98 <marco.eugenio.peressutti@gmail.com>
2025-12-27 17:03:00 +0100

    [enmach] Rewrites CMakeLists.txt and adds library with rotors and reflectors

---
+++ include/enmach/utils.hpp
@@ -0,0 +1,27 @@
+#ifndef ENMACH_UTILS_HPP_
+#define ENMACH_UTILS_HPP_
+#include <string_view>
+
+namespace enmach::rotors
+{
+  inline constexpr std::string_view i     = "ekmflgdqvzntowyhxuspaibrcj";
+  inline constexpr std::string_view ii    = "ajdksiruxblhwtmcqgznpyfvoe";
+  inline constexpr std::string_view iii   = "bdfhjlcprtxvznyeiwgakmusqo";
+  inline constexpr std::string_view iv    = "esovpzjayquirhxlnftgkdcmwb";
+  inline constexpr std::string_view v     = "vzbrgityupsdnhlxawmjqofeck";
+  inline constexpr std::string_view vi    = "jpgvoumfyqbenhzrdkasxlictw";
+  inline constexpr std::string_view vii   = "nzjhgrcxmyswboufaivlpekqdt";
+  inline constexpr std::string_view viii  = "fkqhtlxocbjspdzramewniuygv";
+  inline constexpr std::string_view beta  = "leyjvcnixwpbqmdrtakzgfuhos";
+  inline constexpr std::string_view gamma = "fsokanuerhmbtiycwlqpzxvgjd";
+} // namespace enmach::rotors
+
+namespace enmach::reflectors
+{
+  inline constexpr std::string_view b      = "yruhqsldpxngokmiebfzcwvjat";
+  inline constexpr std::string_view c      = "fvpjiaoyedrzxwgctkuqsbnmhl";
+  inline constexpr std::string_view b_dunn = "enkqauywjicopblmdxzvfthrgs";
+  inline constexpr std::string_view c_dunn = "rdobjntkvehmlfcwzaxgyipsuq";
+} // namespace enmach::reflectors
+
+#endif // ENMACH_UTILS_HPP_
