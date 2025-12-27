a73196935c4e4ede5243f7a5cf28760dd6516fd4
marcope-98 <marco.eugenio.peressutti@gmail.com>
2025-12-27 17:00:37 +0100

    [status] Resets repository to clear working environment

--- src/Logger.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by peres_hplixoz on 14/06/2020.
-//
-
-#include "Logger.h"
-
-
--- src/Reflector.cpp
+++
@@ -1,9 +0,0 @@
-#include "Reflector.h"
-
-void Reflector::standardReflector()
-{
-    for (int i = 0; i < size; ++i)
-    {
-        data[i] = (size - 1) - i*2;
-    }
-}
