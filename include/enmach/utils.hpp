2296d3eb5b62c497df2f79af8609de0e0437e091
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-01-26 12:36:58 +0100

    [enmach] Deletes unused types.hpp file and clears deprecated content of utils.hpp

--- include/enmach/utils.hpp
+++ include/enmach/utils.hpp
@@ -1,32 +1,5 @@
 #ifndef ENMACH_UTILS_HPP_
 #define ENMACH_UTILS_HPP_
 
-#include <string_view>
-
-#include "enmach/types.hpp"
-
-namespace enmach::rotors
-{
-  using namespace std::literals;
-  inline constexpr Rotor I     = "ekmflgdqvzntowyhxuspaibrcj"sv;
-  inline constexpr Rotor II    = "ajdksiruxblhwtmcqgznpyfvoe"sv;
-  inline constexpr Rotor III   = "bdfhjlcprtxvznyeiwgakmusqo"sv;
-  inline constexpr Rotor IV    = "esovpzjayquirhxlnftgkdcmwb"sv;
-  inline constexpr Rotor V     = "vzbrgityupsdnhlxawmjqofeck"sv;
-  inline constexpr Rotor VI    = "jpgvoumfyqbenhzrdkasxlictw"sv;
-  inline constexpr Rotor VII   = "nzjhgrcxmyswboufaivlpekqdt"sv;
-  inline constexpr Rotor VIII  = "fkqhtlxocbjspdzramewniuygv"sv;
-  inline constexpr Rotor BETA  = "leyjvcnixwpbqmdrtakzgfuhos"sv;
-  inline constexpr Rotor GAMMA = "fsokanuerhmbtiycwlqpzxvgjd"sv;
-} // namespace enmach::rotors
-
-namespace enmach::reflectors
-{
-  using namespace std::literals;
-  inline constexpr Reflector B      = "yruhqsldpxngokmiebfzcwvjat"sv;
-  inline constexpr Reflector C      = "fvpjiaoyedrzxwgctkuqsbnmhl"sv;
-  inline constexpr Reflector B_DUNN = "enkqauywjicopblmdxzvfthrgs"sv;
-  inline constexpr Reflector C_DUNN = "rdobjntkvehmlfcwzaxgyipsuq"sv;
-} // namespace enmach::reflectors
 
 #endif // ENMACH_UTILS_HPP_
