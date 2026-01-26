8fff5b79f43116ce8ec3f05f3fb007255861282a
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-01-26 15:32:54 +0100

    [main] Adds simple example in main to verify basic functioning of enigma machine

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
-#include "enmach/EnigmaMachine.hpp"
+#include "enmach/enmach.hpp"
+#include <iostream>
+#include <string_view>
+
+
+using namespace std::literals;
+struct Plugboard
+
+{
+  constexpr static std::string_view value = "dbcatuqmilkjhvozgrsefnwxyp"sv;
+  constexpr auto                    operator()(char letter) const -> char { return this->value.at(enmach::ETW.find(letter)); }
+};
 
 int main(void)
 {
+  enmach::Enigma1<Plugboard,
+                  enmach::reflector_tags::UKW_B,
+                  enmach::rotor_tags::II, enmach::rotor_tags::V, enmach::rotor_tags::III>
+      em;
+  em.setRingstellung('d', 'k', 'x');
+  em.setInitialRotorPosition('y', 'w', 'y');
+  constexpr std::string_view message = "znapq";
+  for (const auto &elem : message)
+  {
+    em.increment();
+    std::cout << em.exec(elem);
+  }
+  std::cout << "\n";
   return 0;
 }
