f577a84dd68b47db874f3413bd443c918311c8d3
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-01-26 12:46:18 +0100

    [main] Clears existing test example with deprecated implementation

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
 #include "enmach/EnigmaMachine.hpp"
-#include "enmach/utils.hpp"
-
-#include <iostream>
 
 int main(void)
 {
-  enmach::EnigmaMachine em{enmach::rotors::i, enmach::rotors::ii, enmach::rotors::iii, enmach::reflectors::b};
-  for (std::size_t i{}; i < 32; ++i)
-  {
-    char                        letter = em.exec('g');
-    enmach::RotorsConfiguration a      = em.getRotorsConfigurations();
-    std::cerr << std::get<0>(a) << " " << std::get<1>(a) << " " << std::get<2>(a) << "\n";
-    (void)letter;
-  }
   return 0;
 }
