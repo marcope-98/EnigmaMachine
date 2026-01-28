6f75bb8b4e6e082d1b398ce44dc7a61323898c68
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-01-28 10:22:34 +0100

    [enmach/Rotor] Improves code size by fusing ringstellung_ and position_ member variables into internal_position private member variable

--- include/enmach/Rotor.hpp
+++ include/enmach/Rotor.hpp
@@ -15,15 +15,15 @@ namespace enmach::rotor_tags
                                                                rvalue = { "\x14\x16\x18\x06\x00\x03\x05\x0f\x15\x19\x01\x04\x02\x0a\x0c\x13\x07\x17\x12\x0b\x11\x08\x0d\x10\x0e\x09" }; };
   struct II    { constexpr static std::array<std::uint8_t, 27> fvalue = { "\x00\x09\x03\x0a\x12\x08\x11\x14\x17\x01\x0b\x07\x16\x13\x0c\x02\x10\x06\x19\x0d\x0f\x18\x05\x15\x0e\x04" }, 
                                                                rvalue = { "\x00\x09\x0f\x02\x19\x16\x11\x0b\x05\x01\x03\x0a\x0e\x13\x18\x14\x10\x06\x04\x0d\x07\x17\x0c\x08\x15\x12" }; 
-                 constexpr static std::string_view notch    = "r"sv; };
+                 constexpr static std::string_view             notch    = "r"sv; };
   struct III   { constexpr static std::array<std::uint8_t, 27> fvalue = { "\x01\x03\x05\x07\x09\x0b\x02\x0f\x11\x13\x17\x15\x19\x0d\x18\x04\x08\x16\x06\x00\x0a\x0c\x14\x12\x10\x0e" }, 
                                                                rvalue = { "\x13\x00\x06\x01\x0f\x02\x12\x03\x10\x04\x14\x05\x15\x0d\x19\x07\x18\x08\x17\x09\x16\x0b\x11\x0a\x0e\x0c" }; 
-                 constexpr static std::string_view notch    = "w"sv; };
+                 constexpr static std::string_view             notch    = "w"sv; };
   struct IV    { constexpr static std::array<std::uint8_t, 27> fvalue = { "\x04\x12\x0e\x15\x0f\x19\x09\x00\x18\x10\x14\x08\x11\x07\x17\x0b\x0d\x05\x13\x06\x0a\x03\x02\x0c\x16\x01" }, 
                                                                rvalue = { "\x07\x19\x16\x15\x00\x11\x13\x0d\x0b\x06\x14\x0f\x17\x10\x02\x04\x09\x0c\x01\x12\x0a\x03\x18\x0e\x08\x05" }; };
   struct V     { constexpr static std::array<std::uint8_t, 27> fvalue = { "\x15\x19\x01\x11\x06\x08\x13\x18\x14\x0f\x12\x03\x0d\x07\x0b\x17\x00\x16\x0c\x09\x10\x0e\x05\x04\x02\x0a" }, 
                                                                rvalue = { "\x10\x02\x18\x0b\x17\x16\x04\x0d\x05\x13\x19\x0e\x12\x0c\x15\x09\x14\x03\x0a\x06\x08\x00\x11\x0f\x07\x01" }; 
-                 constexpr static std::string_view notch    = "f"sv; };
+                 constexpr static std::string_view             notch    = "f"sv; };
   struct VI    { constexpr static std::array<std::uint8_t, 27> fvalue = { "\x09\x0f\x06\x15\x0e\x14\x0c\x05\x18\x10\x01\x04\x0d\x07\x19\x11\x03\x0a\x00\x12\x17\x0b\x08\x02\x13\x16" }, 
                                                                rvalue = { "\x12\x0a\x17\x10\x0b\x07\x02\x0d\x16\x00\x11\x15\x06\x0c\x04\x01\x09\x0f\x13\x18\x05\x03\x19\x14\x08\x0e" }; };
   struct VII   { constexpr static std::array<std::uint8_t, 27> fvalue = { "\x0d\x19\x09\x07\x06\x11\x02\x17\x0c\x18\x12\x16\x01\x0e\x14\x05\x00\x08\x15\x0b\x0f\x04\x0a\x10\x03\x13" }, 
@@ -45,32 +45,43 @@ namespace enmach
   public:
     [[nodiscard]] constexpr auto forward(std::uint8_t index) const -> std::uint8_t
     {
-      index = (index + this->position_ + enmach::ETW.size() - this->ringstellung_) % enmach::ETW.size();
+      index = (index + this->internal_difference) % enmach::ETW.size();
       index = RotorTag::fvalue[index];
-      index = (index + enmach::ETW.size() - this->position_ + this->ringstellung_) % enmach::ETW.size();
+      index = (index + enmach::ETW.size() - this->internal_difference) % enmach::ETW.size();
       return index;
     }
 
     [[nodiscard]] constexpr auto inverse(std::uint8_t index) const -> std::uint8_t
     {
-      index = (index + this->position_ + enmach::ETW.size() - this->ringstellung_) % enmach::ETW.size();
+      index = (index + this->internal_difference) % enmach::ETW.size();
       index = RotorTag::rvalue[index];
-      index = (index + enmach::ETW.size() - this->position_ + this->ringstellung_) % enmach::ETW.size();
+      index = (index + enmach::ETW.size() - this->internal_difference) % enmach::ETW.size();
       return index;
     }
 
     [[nodiscard]] constexpr auto increment(bool condition) -> bool
     {
-      this->position_ = (this->position_ + static_cast<std::uint8_t>(condition)) % enmach::ETW.size();
-      return condition && RotorTag::notch.find(ETW.at(this->position_)) != std::string_view::npos;
+      this->internal_difference = (this->internal_difference + static_cast<std::uint8_t>(condition)) % enmach::ETW.size();
+      return condition && RotorTag::notch.find(ETW.at(this->internal_difference)) != std::string_view::npos;
     }
 
-    constexpr auto setInitialPosition(char initial_position) -> void { this->position_ = static_cast<std::uint8_t>(initial_position - 'a'); }
-    constexpr auto setRingstellung(char ringstellung) -> void { this->ringstellung_ = static_cast<std::uint8_t>(ringstellung - 'a'); }
+    constexpr auto setInitialPosition(char initial_position) -> void
+    {
+      this->position_ = static_cast<std::uint8_t>(initial_position - 'a');
+      this->setInternalDifference();
+    }
+    constexpr auto setRingstellung(char ringstellung) -> void
+    {
+      this->ringstellung_ = static_cast<std::uint8_t>(ringstellung - 'a');
+      this->setInternalDifference();
+    }
 
   private:
+    constexpr auto setInternalDifference() -> void { this->internal_difference = this->position_ + enmach::ETW.size() - this->ringstellung_; }
+
     std::uint8_t position_{};
     std::uint8_t ringstellung_{};
+    std::uint8_t internal_difference{};
   };
 } // namespace enmach
 #endif // ENMACH_ROTOR_HPP_
